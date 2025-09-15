75474d0cf1ef28b5e2b5a0f0f01d6b96d590431f
nikhita-code <nikhita.kalamkar@gmail.com>
2025-09-16 02:29:41 +0530

    Update main.tf

--- env/test/main.tf
+++ env/test/main.tf
@@ -4,12 +4,8 @@ provider "aws" {
 }
 
 
-#ec2-instance sample for terraform destroy
-module "custom_modules" {
+#s3-bucket sample for terraform destroy
+module "test_modules" {
   source = "../../modules/test_modules"
-  ami = var.ami
-  instance_type = var.instance_type
-  key_pair_nm = var.key_pair_nm
-  vpc_id = var.vpc_id
-  subnet_id = var.subnet_id 
+  bucket_name = var.bucket_name
 }

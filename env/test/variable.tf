4fd87fd119dbc819b6a0e8f699530827a5abe31f
nikhita-code <nikhita.kalamkar@gmail.com>
2025-09-16 01:53:33 +0530

    Create variable.tf

---
+++ env/test/variable.tf
@@ -0,0 +1,15 @@
+
+#ec2_instance
+
+variable "ami" {
+   type = string
+}
+
+variable "instance_type" {
+    type = string
+    default = "t2.micro"
+}
+
+variable "key_pair_nm" {
+   type = string
+}

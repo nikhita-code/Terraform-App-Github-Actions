46609ea06c9c3d66f0edc3248d7e4b80234c8b2c
nikhita-code <nikhita.kalamkar@gmail.com>
2025-09-16 02:27:29 +0530

    Update variable.tf

--- modules/test_modules/variable.tf
+++ modules/test_modules/variable.tf
@@ -1,23 +1,3 @@
-#ec2_instance
-
-variable "ami" {
-   type = string
-}
-
-variable "instance_type" {
-    type = string
-    default = "t2.micro"
-}
-
-variable "key_pair_nm" {
-   type = string
-}
-
-variable "vpc_id" {
-   type = string
-}
-
-variable "subnet_id" {
-  description = "Subnet ID where the EC2 instance will be launched"
-  type        = string
+variable "bucket_name" {
+   type = string 
 }
